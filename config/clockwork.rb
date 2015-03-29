a9705f0675b62bab52ddb6e891d8dc7f7336e2b3
Mike Silvis <mikesilvis@gmail.com>
2015-03-29 15:55:59 -0700

    run the espn job every day at midnight

--- config/clockwork.rb
+++ config/clockwork.rb
@@ -3,5 +3,5 @@ require './lib/scheduler'
 
 module Clockwork
   every(1.minute, 'frequent.job') { Worker::RealtimePush.perform_async }
-  every(7.day, 'cache.clear') { Worker::ESPN.perform_async }
+  every(1.day, 'cache.clear', at: '07:30', tz: 'UTC') { Worker::ESPN.perform_async }
 end
