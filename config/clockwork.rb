68b66395cf583b57f53a635dd91be9ffa09f0e2f
Mike Silvis <mikesilvis@gmail.com>
2015-04-19 14:43:31 -0700

    increasing the timer back to 30 seconds

--- config/clockwork.rb
+++ config/clockwork.rb
@@ -2,6 +2,6 @@ require 'clockwork'
 require './lib/scheduler'
 
 module Clockwork
-  every(15.seconds, 'frequent.job') { Worker::RealtimePush.perform_async }
+  every(30.seconds, 'frequent.job') { Worker::RealtimePush.perform_async }
   every(1.day, 'cache.clear', at: '07:30', tz: 'UTC') { Worker::ESPN.perform_async }
 end
